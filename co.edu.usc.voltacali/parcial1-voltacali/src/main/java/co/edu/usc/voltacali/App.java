33ec79d9be503af738b9dccb2627d48ecee40a8d
Mahito2-Kou2 <mariajosesalinasalegria0@gmail.com>
2026-09-24 01:07:52 +0000

    Punto D de enums :D

--- co.edu.usc.voltacali/parcial1-voltacali/src/main/java/co/edu/usc/voltacali/App.java
+++ co.edu.usc.voltacali/parcial1-voltacali/src/main/java/co/edu/usc/voltacali/App.java
@@ -3,9 +3,19 @@ package co.edu.usc.voltacali;
 /* INTENTO 2: Hello world!_SE BORRO EL ANTERIRO*/
 
 public class CargadorVE {
+
 public enum TipoConector {
-    Tipo_1, Tipo_2, CCS2, CHADEMO, GBT
+    TTIPO_1, TIPO_2, CCS2, CHADEMO, GBT
 }
+
+public enum TipoCOnector {
+    MURAL, PEDESTAL, RAPIDO_DC, ULTRARRAPIDO, PORTATIL, BIDIRECCIONAL_V2G
+}
+
+public enum Ubicacion {
+    CENTRO_COMERCIAL, UNIVERSIDAD, ESTACION_SERVICIO, PARQUEADERO_PUBLICO, RESIDENCIAL, HOTEL, TERMINAL, FLOTA_CORPORATIVA
+}
+
 }
 public final class App {
     private App() {
