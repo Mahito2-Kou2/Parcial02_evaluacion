a5c85b3097f6385fb6509df5dd630c9473f0adb4
Mahito2-Kou2 <mariajosesalinasalegria0@gmail.com>
2026-09-24 00:54:47 +0000

    Primera instancia:

---
+++ co.edu.usc.voltacali/parcial1-voltacali/src/main/java/co/edu/usc/voltacali/App.java
@@ -0,0 +1,16 @@
+package co.edu.usc.voltacali;
+
+/* INTENTO 2: Hello world!_SE BORRO EL ANTERIRO*/
+
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
