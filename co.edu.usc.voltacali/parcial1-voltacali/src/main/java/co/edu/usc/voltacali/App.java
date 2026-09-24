eeee9411bdf0094df8c3fd429ccfe70c1ad01295
Mahito2-Kou2 <mariajosesalinasalegria0@gmail.com>
2026-09-24 04:13:28 +0000

    arreglos

--- co.edu.usc.voltacali/parcial1-voltacali/src/main/java/co/edu/usc/voltacali/App.java
+++ co.edu.usc.voltacali/parcial1-voltacali/src/main/java/co/edu/usc/voltacali/App.java
@@ -2,84 +2,96 @@ package co.edu.usc.voltacali;
 
 import java.util.Vector;
 
-/* INTENTO 2: Hello world!_SE BORRO EL ANTERIRO*/
+/** Clase principal del cargador VE. */
+class CargadorVE {
+    /** Atributos */
+    private String fabricante;
+    private int anioInstalacion;
+    private int voltajeNominal;
+    private TipoCargador tipoCargador;
+    private TipoConector tipoConector;
+    private int numeroConectores;
+    private int puestosParqueo;
+    private double potenciaMaxima;
+    private Ubicacion ubicacion;
+    private double potenciaActual;
 
-/**Clase**/
+    private static int totalCargadores = 0;
+    private static int contadorRegistros = 0;
 
-public class CargadorVE {
+    public static final double LIMITE_RED = 50.0;
+    public static final double INCREMENTO_DEFECTO = 5.0;
 
-/** Atributos**/
+    private final Vector<RegistroSesion> registro = new Vector<>();
 
-private String fabricante;
-private String anioIntalacion;
-private String voltajeNominal;
-private TipoCargador tipoCargador;
-private TipoConector tipoConector;
-private static int totalCargadores = 0;
-private static int contadorRegistros = 0;
-public static final double LIMITE_RED = 50.0;
-public static final double INCREMENTO_DEFECTO = 5.0;
-
-/* Punto D*/
-public enum TipoConector {
-    TTIPO_1, TIPO_2, CCS2, CHADEMO, GBT
-}
-
-public enum TipoCargador{
-    MURAL, PEDESTAL, RAPIDO_DC, ULTRARRAPIDO, PORTATIL, BIDIRECCIONAL_V2G
-}
-
-public enum Ubicacion {
-    CENTRO_COMERCIAL, UNIVERSIDAD, ESTACION_SERVICIO, PARQUEADERO_PUBLICO, RESIDENCIAL, HOTEL, TERMINAL, FLOTA_CORPORATIVA
-}
-
-private final Vector<RegistroSesion> registro = new Vector<RegistroSesion>();
-
-public class RegistroSesion {
-    private final int numero;
-    private final String evento;
-    private final boolean valido;
-    private final string fabricante;
-    private final int anioInstalacion;
-    private final double potenciaActual;
-
-    public RegistroSesion(String evento, boolean valido){
-        this.evento = evento;
-        this.valido = valido;
-        this.fabricante = CargadorVE.this.fabricante;
-        this.anioInstalacion = CargadorVE.this.anioInstalacion;
-        this.potenciaActual = CargadorVE.this.potenciaActual;
-        
-        /** Contador **/
-        contadorRegistros++;
-        this.numero = contadorRegistros;
-    
+    public enum TipoConector {
+        TTIPO_1, TIPO_2, CCS2, CHADEMO, GBT
     }
 
-    public String describir(){
-        return "-" + numero + "-" + fabricante + "(" + anioInstalacion + ")"
-    + " | " + potenciaActual + "kw"
-    + " | " + (valido ? "VALIDO" : "INVADO")
-    + " | " + evento;
-
-    } 
-
-    public CargadorVE(String fabricante, int anioInstalacion, int voltajeNominal, TipoConector tipoConector, TipoCargador tipocargador, int numeroConectores, int puestosParqueo, double potenciaMaxima, Ubicacion ubicacion){
-    this.fabricante = fabricante;
-    this.anioInstalacion = anioInstalacion;
-    this.voltajeNominal = voltajeNominal;
-    this.tipoConector = tipoConector;
-    this.puestosParqueo = puestosParqueo;
-    this.potenciaMaxima = potenciaMaxima;
-    this.ubicacion = ubicacion;
-    this.potenciaActual = 0;
-
-    /**Contador**/
-    
+    public enum TipoCargador {
+        MURAL, PEDESTAL, RAPIDO_DC, ULTRARRAPIDO, PORTATIL, BIDIRECCIONAL_V2G
     }
 
-}
+    public enum Ubicacion {
+        CENTRO_COMERCIAL, UNIVERSIDAD, ESTACION_SERVICIO, PARQUEADERO_PUBLICO,
+        RESIDENCIAL, HOTEL, TERMINAL, FLOTA_CORPORATIVA
+    }
 
+    public class RegistroSesion {
+        private final int numero;
+        private final String evento;
+        private final boolean valido;
+        private final String fabricante;
+        private final int anioInstalacion;
+        private final double potenciaActual;
+
+        public RegistroSesion(String evento, boolean valido) {
+            this.evento = evento;
+            this.valido = valido;
+            this.fabricante = CargadorVE.this.fabricante;
+            this.anioInstalacion = CargadorVE.this.anioInstalacion;
+            this.potenciaActual = CargadorVE.this.potenciaActual;
+
+            contadorRegistros++;
+            this.numero = contadorRegistros;
+        }
+
+        public String describir() {
+            return "-" + numero + "-" + fabricante + "(" + anioInstalacion + ")"
+                    + " | " + potenciaActual + "kw"
+                    + " | " + (valido ? "VALIDO" : "INVALIDO")
+                    + " | " + evento;
+        }
+    }
+
+    public CargadorVE(String fabricante, int anioInstalacion, int voltajeNominal,
+            TipoConector tipoConector, TipoCargador tipoCargador, int numeroConectores,
+            int puestosParqueo, double potenciaMaxima, Ubicacion ubicacion) {
+        this.fabricante = fabricante;
+        this.anioInstalacion = anioInstalacion;
+        this.voltajeNominal = voltajeNominal;
+        this.tipoConector = tipoConector;
+        this.tipoCargador = tipoCargador;
+        this.numeroConectores = numeroConectores;
+        this.puestosParqueo = puestosParqueo;
+        this.potenciaMaxima = potenciaMaxima;
+        this.ubicacion = ubicacion;
+        this.potenciaActual = 0;
+        totalCargadores++;
+    }
+
+    public CargadorVE(String fabricante, int anioInstalacion, double potenciaMaxima) {
+        this(fabricante, anioInstalacion, 220, TipoConector.TIPO_2,
+                TipoCargador.PEDESTAL, 1, 1, potenciaMaxima, Ubicacion.PARQUEADERO_PUBLICO);
+    }
+
+    public void registrarEvento(String evento, boolean valido) {
+        registro.add(new RegistroSesion(evento, valido));
+    }
+
+    public Vector<RegistroSesion> getRegistro() {
+        return registro;
+    }
 }
 
 public final class App {
